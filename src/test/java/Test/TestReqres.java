cedad8828d7a78e048b26008237e73bd4ac00cc4
ACusmir <andrian.cusmir@orange.com>
2024-05-18 12:41:08 +0300

    add first test

--- src/test/java/Test/TestReqres.java
+++ src/test/java/Test/TestReqres.java
@@ -1,11 +1,17 @@
 package Test;
 
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
 
 @Test
 public class TestReqres {
-    public  void testTestNG(){
-        System.out.println("hello world");
-    }
+    public void testListUsers() {
 
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then().assertThat()
+                .statusCode(200);
+    }
 }
